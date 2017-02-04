09e6062f69b39a827f6c7a9928f86386ccd7d134
Ravithej Chikkala <rchikka@us.ibm.com>
2017-02-04 13:33:51 -0600

    db pg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var jobDb = require('./db/jobs');
-
+var pg = require('pg');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
