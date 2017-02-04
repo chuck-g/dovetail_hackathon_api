b3de2ee7fd2337ce2b42bbbcf45b46aaf0b35513
Ravithej Chikkala <rchikka@us.ibm.com>
2017-02-04 13:29:26 -0600

    db

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/times', function(request, response) {
       result += i + ' ';
   response.send(result);
 });
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.get('/api/jobs', jobDb.query);
 app.post('/api/jobs', jobDb.createRecord);
